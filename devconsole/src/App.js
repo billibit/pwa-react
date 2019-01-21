f72805571b39accb3cd4ce38e23bf2c87b461104
billbit <billibit@hotmail.com>
2019-01-20 17:54:59 -0800

    Initialized the project

---
+++ devconsole/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Please add to your home screen and it will work like a native app!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            what is PWA?
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
